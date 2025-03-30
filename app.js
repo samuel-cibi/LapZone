003f4c7465ef7e10e0692e898f6fc1eebebd8f85
samuel-cibi <samuelcibi@gmail.com>
2025-03-30 19:02:45 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const button=document.getElementById('ABOUT US')
+button.addEventListener("click",doSomething)
+function doSomething()
+{
+    alert("THIS WEBSITE IS DESIGNED BY RAJKRISHNA AND SAMUEL")
+}
